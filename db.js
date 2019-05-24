895523325edfe7ca92a0baa985511822ed1e143c
Pablo Moises Mazariegos Pinto <34450035+PabloMazariegos@users.noreply.github.com>
2019-05-23 21:17:27 -0600

    Update db.js

--- db.js
+++ db.js
@@ -4,7 +4,7 @@ var cnx = {
   sockePath: 'escuelapp-240604:us-central1:bdescuelapp',
   user: 'root',
   password: '159515',
-  database: 'BD_ESCUELAPP'
+  database: 'BD_ESCUELAPP',
 }
 
 var SQLpool = mysql.createPool(cnx);
